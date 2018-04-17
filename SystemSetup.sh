8cdb1d7bc1314cd2c19283333057c8421cb89f98
xelous <xelous@hotmail.co.uk>
2018-04-17 17:00:20 +0100

    Added a System setup script into the repo, to assist in boot-strapping the use and development on new machines.

---
+++ SystemSetup.sh
@@ -0,0 +1,23 @@
+sudo apt-get update & sudo apt-get upgrade
+sudo apt-get install -y python3 python3-pip git autoconf automake libtool curl make g++ unzip
+sudo pip3 install flask
+hostname -f > hostname.txt
+sudo apt-get install -y mysql-server
+sudo mysql_secure_installation
+sudo ldconfig
+cd ~
+git clone http://github.com/google/protobuf.git
+cd protobuf
+./autogen.sh
+./configure
+make -j8
+make check
+sudo make install
+sudo ldconfig
+cd python
+cd python ./setup.py install
+sudo ldconfig
+sudo pip3 install --upgrade pip
+sudo pip3 install mysql-connector
+
+
